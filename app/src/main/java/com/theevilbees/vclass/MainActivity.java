4a320c035075985d458a05d4f2dec6f0848c2c6b
SAMANVAYA <samanvaya.yagsen@gmail.com>
2020-07-21 02:04:50 +0530

    basic login

--- app/src/main/java/com/theevilbees/vclass/MainActivity.java
+++ app/src/main/java/com/theevilbees/vclass/MainActivity.java
@@ -1,8 +1,10 @@
 package com.theevilbees.vclass;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GoogleSignInAccount user = GoogleSignIn.getLastSignedInAccount(this);
+        if(user==null)
+        {
+            Intent i= new Intent(getApplicationContext(), signIn.class);
+            startActivity(i);
+        }
+        else
+        {
+            if(getType()==1)
+            {
+                Intent i= new Intent(getApplicationContext(),student.class);
+                startActivity(i);
+
+            }
+            else
+            {
+                Intent i= new Intent(getApplicationContext(),teacher.class);
+                startActivity(i);
+
+            }
+        }
+
     }
+    int getType()
+    {
+        return 0;
+    }
+
 }
