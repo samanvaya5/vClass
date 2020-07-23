f3e8b0ac64dfaf00b8d5f60243f2d4779136f603
SAMARTH <sshankrit1206@gmail.com>
2020-07-23 21:23:04 +0530

    added splashscreen

--- app/src/main/java/com/theevilbees/vclass/MainActivity.java
+++ app/src/main/java/com/theevilbees/vclass/MainActivity.java
@@ -1,12 +1,16 @@
 package com.theevilbees.vclass;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static int splashtime = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent homeIndent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(homeIndent);
+                finish();
+            }
+        },splashtime);
     }
 }
